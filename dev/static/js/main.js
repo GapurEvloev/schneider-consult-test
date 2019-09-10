ce2fa724c62f38db96b6a772c95b38303d38c785
GAPUR EVLOEV <g.evloev@itfsystems.ru>
2019-09-10 18:15:52 +0300

    первое задание по js

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,21 @@
-$(document).ready(function () {
-    svg4everybody({});
-});
+function formatDate(date) {
+
+    let dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
+
+    let mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+
+    let yy = date.getFullYear() % 100;
+    if (yy < 10) yy = '0' + yy;
+
+    return `${dd}.${mm}.${yy}`;
+}
+(function() {
+date = new Date();
+m = new Date();
+if (date.getDay()) { m.setDate(date.getDate() + 8 - date.getDay()) } else { m.setDate(date.getDate() + 1) }
+
+
+document.querySelector('.header__info-item').innerText += " " + formatDate(m) + "!";
+})();
